d1d3ee352384c7e7ae02b0b71d12bde28e7ed019
Z0Xf7if2Xi4V <duplosneg0@gmail.com>
2026-06-13 17:10:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ DB_NAME = DATA_DIR / "bot.db"
 
 # Webhook настройки
 WEBHOOK_HOST = f"https://{os.getenv('DOMAIN')}" # Ваш домен (обязательно HTTPS)
-WEBHOOK_PORT = int(os.getenv("PORT", "8443")) # Порт для webhook (обычно 443, 8443, 80)
+WEBHOOK_PORT = int(os.getenv("PORT", "3000")) # Порт для webhook (обычно 443, 8443, 80)
 WEBHOOK_LISTEN = "0.0.0.0"  # Адрес для прослушивания
 WEBHOOK_URL_PATH = f"/webhook/{TOKEN}"  # Путь для webhook
 
